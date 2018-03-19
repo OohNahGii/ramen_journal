5a3aff6a61a9e685f41a3a6e188fa5dea468de6c
Grant Ramil <grant.ramil@veeva.com>
2018-03-19 11:25:06 -0700

    CORS for local testing

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const entries = require('./entries.js');
+const config = require('./config.json');
 const app = express();
 
 function isNaturalNumber(str) {
   let num = Math.floor(Number(str));
   return num !== Infinity && String(num) === str && num >= 0;
 }
+
+// For testing purposes
+if (config.local) {
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    next();
+  });
+}
+
 
 app.get('/entries', (req, res) => {
   const page = req.query.page;
